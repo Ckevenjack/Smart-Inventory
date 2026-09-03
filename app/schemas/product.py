a1ff8aa4a4ef8f6a5bc6232f02db8a9393233adc
ckeven <ckevenjack@gmail.com>
2026-09-03 05:41:57 +0300

    feat: add product validation

--- app/schemas/product.py
+++ app/schemas/product.py
@@ -1,16 +1,28 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
 from typing import Optional
 
 class ProductCreate(BaseModel):
-    name: str
-    category: str
-    price: float
-    stock: int
-    minimum_stock: int
+    name: str = Field(min_length=2, max_length=100)
+    category: str = Field(min_length=2, max_length=50)
+    price: float = Field(gt=0)
+    stock: int = Field(ge=0)
+    minimum_stock: int= Field(ge=0)
 
 class ProductUpdate(BaseModel):
-    name: Optional[str] = None
-    category: Optional[str] = None
-    price: Optional[float] = None
-    stock: Optional[int] = None
-    minimum_stock: Optional[int] = None
+    name: Optional[str] = Field(default=None, min_length=2, max_length=100)
+    category: Optional[str] = Field(default=None, min_length=2, max_length=50)
+    price: Optional[float] = Field(default=None, gt=0)
+    stock: Optional[int] = Field(default=None, ge=0)
+    minimum_stock: Optional[int] = Field(default=None, ge=0)
+
+    @field_validator(
+        "name",
+        "category",
+        "price",
+        "stock",
+        "minimum_stock")
+    def product_validator(cls, value):
+        if value is None:
+            raise ValueError("Field cannot be null")
+            
+        return value
