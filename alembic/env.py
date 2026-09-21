c21cedc4507b73e4b2195aac22549cdb709a421a
ckeven <ckevenjack@gmail.com>
2026-09-21 09:28:07 +0300

    test: configure test database for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -6,12 +6,23 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.database import Base, DATABASE_URL
+from dotenv import load_dotenv
+import os
 from app.models.product import Product
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
+
+load_dotenv()
+
 config = context.config
-config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+if os.getenv("USE_TEST_DB") == "true":
+    database_url = os.getenv("TEST_DATABASE_URL")
+else:
+    database_url = DATABASE_URL
+
+config.set_main_option("sqlalchemy.url", database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
